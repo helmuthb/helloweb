76fc083b37d629ab5b7bc325c7dac29c626983da
Helmuth Breitenfellner <helmuth@breitenfellner.at>
2022-03-25 17:50:59 +0100

    Add environment output

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Flask with docker!'
+    nl = "\n"
+    return f"""
+      <html>
+       <head>
+        <title>Hello World from a Container</title>
+       </head>
+       <body>
+        <h1>Hello World!</h1>
+        This is Python running in a container.
+        <h2>Environment</h2>
+        <pre>{nl.join([k+'='+v for k, v in dict(os.environ).items()])}</pre>
+       </body>
+      </html>
+    """
